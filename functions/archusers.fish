a460c973268f935bf16c2a4c045fd33f35a8af63
mindonly <thesanch@gmail.com>
2018-10-20 11:46:58 -0400

    delete unneeded files.

--- functions/archusers.fish
+++
@@ -1,8 +0,0 @@
-# Defined in /tmp/fish.PNwgOf/archusers.fish @ line 2
-function archusers
-	set boxes arch01 arch02 arch03 arch04 arch05 arch06 arch07 arch08 arch09 arch10
-    for box in $boxes
-        printf '\n%s\n%s\n' $box "------"
-        ssh $box "uptime; who"
-    end
-end
